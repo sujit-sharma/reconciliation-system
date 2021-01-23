2eb5724d8b6d3ada9a7fcd1d159f1483378f22c0
sujit <sujit.sharma@clusus.com>
2021-01-23 21:21:34 +0545

    Repoisitory test written successfully

---
+++ domain/src/test/java/com/sujit/JsonTransactionTest.java
@@ -0,0 +1,32 @@
+package com.sujit;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonTransactionTest {
+
+    @Test
+    void WhenInitializedJsonTxnShouldReturnAssignValue() {
+        String date  = "25/01/2020";
+        String reference  = "TR-47884222201";
+        Double amount  = 124.20;
+        Currency currency  = Currency.getInstance("JOD");
+        String purpose  =  "donation";
+        JsonTransaction jsonTransaction = new JsonTransaction(date,
+                reference,
+                amount,
+                Currency.getInstance("JOD"),
+                "donation");
+
+        assertEquals(date, jsonTransaction.getDate());
+        assertEquals(reference, jsonTransaction.getReference());
+        assertEquals(reference, jsonTransaction.getReference());
+        assertEquals(amount, jsonTransaction.getAmount());
+        assertEquals(currency , jsonTransaction.getCurrencyCode());
+        assertEquals(purpose, jsonTransaction.getPurpose());
+    }
+
+}
---
+++ repository/src/test/java/com/sujit/ReconciliationDAOImplTest.java
@@ -0,0 +1,78 @@
+package com.sujit;
+
+import com.sujit.channel.Channel;
+import com.sujit.channel.FileSystemChannel;
+import com.sujit.dataformat.ApacheCsvParser;
+import com.sujit.dataformat.GoogleJsonParser;
+import com.sujit.dataformat.Parser;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.util.Currency;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReconciliationDAOImplTest {
+
+    @Test
+    void whenExecuteFindAllWithCsvFileAndCsvParserThenShouldReturnListOfTransactions() {
+        Parser parser = new ApacheCsvParser();
+        File file = new File( "."+File.separator + "src" + File.separator+"test"+File.separator+"resources"+File.separator+"input.csv");
+        Channel channel = new FileSystemChannel(parser, file);
+
+        ReconciliationDAOImpl dao = new ReconciliationDAOImpl(channel);
+        List<Transaction> result  =  dao.findAll();
+
+        assertEquals(6, result.size());
+    }
+
+    @Test
+    void whenExecuteFindAllWithJsonFileAndJsonParserThenShouldReturnListOfTransactions() {
+        Parser parser = new GoogleJsonParser();
+        File file = new File( "."+File.separator + "src" + File.separator+"test"+File.separator+"resources"+File.separator+"input.json");
+        Channel channel = new FileSystemChannel(parser, file);
+
+        ReconciliationDAOImpl dao = new ReconciliationDAOImpl(channel);
+        List<Transaction> result  =  dao.findAll();
+
+        assertEquals(7, result.size());
+    }
+    @Test
+    void givenARowWhenExecuteSaveRowThenShouldPersistRow() throws IOException {
+        Parser parser = new ApacheCsvParser();
+        File file = new File( "."+File.separator + "src" + File.separator+"test"+File.separator+"resources"+File.separator+"fileChannelOutput.csv");
+        Channel channel = new FileSystemChannel(parser, file);
+        ReconciliationDAOImpl dao = new ReconciliationDAOImpl(channel);
+        final String COMMA = ",";
+        Transaction expected =
+                new Transaction(
+                        "TR-47884222201",
+                        "online transfer",
+                        Double.parseDouble("140"),
+                        Currency.getInstance("USD"),
+                        "donation",
+                        LocalDate.parse("2020-01-20"),
+                        'D');
+        String row =
+                expected.getTransId()
+                        + COMMA
+                        + expected.getAmount()
+                        + COMMA
+                        + expected.getCurrencyCode()
+                        + COMMA
+                        + expected.getDate();
+        dao.saveRow(row);
+        byte[] actual = Files.readAllBytes(file.toPath());
+
+        assertEquals(row + "\n", new String(actual));
+
+        file.delete();
+
+
+    }
+
+}
